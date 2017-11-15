654a4c4406abb643da0a172527f7049a27b842db
Benjamin Pollack <benjamin@bitquabit.com>
2017-11-14 19:53:39 -0500

    Just use fsnotify for Mac and Linux
    fsnotify works just fine for both Mac and Linux, so just use that.
    This also avoids solving a build issue that exists with fsevents under
    macOS High Sierra.

---
+++ watch/watch.go
@@ -0,0 +1,44 @@
+package watch
+
+import (
+	"os"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func Watch(restart string, done <-chan struct{}, change func()) error {
+	lastStat, err := os.Stat(restart)
+
+	if err != nil {
+		return err
+	}
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+
+	defer watcher.Close()
+
+	err = watcher.Add(restart)
+	if err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case <-watcher.Events:
+			cur, err := os.Stat(restart)
+			if err != nil {
+				return err
+			}
+
+			if cur.ModTime().After(lastStat.ModTime()) {
+				change()
+			}
+		case <-done:
+			return nil
+		}
+	}
+
+}
